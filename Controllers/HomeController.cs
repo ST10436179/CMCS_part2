5737374f30ed6a62210109153ffd79aa541a0e82
ST10436179 <162137286+ST10436179@users.noreply.github.com>
2025-11-21 23:22:55 +0200

    Refactor HomeController to use new context and environment

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -1,32 +1,23 @@
-using System.Diagnostics;
-using CMCSCopilot.Models;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using ContractMonthlyClaimSystem.Models;
 
-namespace CMCSCopilot.Controllers
+namespace ContractMonthlyClaimSystem.Controllers
 {
     public class HomeController : Controller
     {
-        private readonly ILogger<HomeController> _logger;
+        private readonly AppDbContext _context;
+        private readonly IWebHostEnvironment _environment;
 
-        public HomeController(ILogger<HomeController> logger)
+        public HomeController(AppDbContext context, IWebHostEnvironment environment)
         {
-            _logger = logger;
+            _context = context;
+            _environment = environment;
         }
 
         public IActionResult Index()
         {
             return View();
-        }
-
-        public IActionResult Privacy()
-        {
-            return View();
-        }
-
-        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
-        public IActionResult Error()
-        {
-            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
         }
     }
 }
